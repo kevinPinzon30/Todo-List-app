a54403668d84d8b4fccf23242251021724a78f2b
kevinPinzon <149128073+kevinPinzon30@users.noreply.github.com>
2024-11-24 16:18:01 -0600

    firebase was implemented

---
+++ lib/core/services/firestore_db.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreDatabase {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  Future<QuerySnapshot> getCollection(String collectionName) async {
+    return await _firebaseFirestore.collection(collectionName).get();
+  }
+
+  Future<DocumentSnapshot> getDocument(
+      String collectionName,
+      String documentId,
+      ) async {
+    return await _firebaseFirestore
+        .collection(collectionName)
+        .doc(documentId)
+        .get();
+  }
+
+  // get document from collection where field == value
+  Future<DocumentSnapshot?> getDocumentWithQuery(
+      String collectionName,
+      Object field,
+      Object value,
+      ) async {
+    final QuerySnapshot querySnapshot = await _firebaseFirestore
+        .collection(collectionName)
+        .where(field, isEqualTo: value)
+        .limit(1)
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      return querySnapshot.docs.first;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> addDocument(
+      String collectionName,
+      Map<String, dynamic> data,
+      ) async {
+    await _firebaseFirestore.collection(collectionName).add(data);
+  }
+
+  Future<void> updateDocument(
+      String collectionName,
+      String documentId,
+      Map<String, dynamic> data,
+      ) async {
+    await _firebaseFirestore
+        .collection(collectionName)
+        .doc(documentId)
+        .update(data);
+  }
+
+  Future<DocumentSnapshot<Object?>> getDocumentFromReference(
+      DocumentReference documentReference,
+      ) async {
+    return await documentReference.get();
+  }
+}
